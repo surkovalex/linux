3219da44c625889150c84bc130db50567c47cc1d
bo.yang <bo.yang@amlogic.com>
2014-08-11 17:00:04 +0800

    PD #93541: fix compiling warning

--- drivers/amlogic/ion_dev/dev_ion.c
+++ drivers/amlogic/ion_dev/dev_ion.c
@@ -46,7 +46,7 @@ static struct resource memobj;
 int dev_ion_probe(struct platform_device *pdev) {
     int err;
     int i;
-    
+
     struct resource *res;
     struct device_node	*of_node = pdev->dev.of_node;
     const void *name;
@@ -92,11 +92,11 @@ int dev_ion_probe(struct platform_device *pdev) {
 			printk("\ndev_ion memory resource undefined4.\n");
         		return -EFAULT;
 		}
-		
-		
+
+
 		res->start = (phys_addr_t)get_reserve_block_addr(i)+offset;
     		res->end = res->start+ size-1;
-		
+
 	 }
     }
     else
@@ -110,7 +110,7 @@ int dev_ion_probe(struct platform_device *pdev) {
         my_ion_heap[1].type = ION_HEAP_TYPE_CARVEOUT;//ION_HEAP_TYPE_CHUNK;//ION_HEAP_TYPE_CARVEOUT;
         my_ion_heap[1].id = ION_HEAP_TYPE_CARVEOUT;
         my_ion_heap[1].name = "carveout_ion";
-        my_ion_heap[1].base = (ion_phys_addr_t *) res->start;
+        my_ion_heap[1].base = (ion_phys_addr_t) res->start;
         my_ion_heap[1].size = res->end - res->start + 1;
     }
     heaps = kzalloc(sizeof(struct ion_heap *) * num_heaps, GFP_KERNEL);
@@ -133,7 +133,7 @@ int dev_ion_probe(struct platform_device *pdev) {
     }
     platform_set_drvdata(pdev, idev);
     return 0;
-err: 
+err:
 	for (i = 0; i < num_heaps; i++) {
         if (heaps[i])
             ion_heap_destroy(heaps[i]);
