3507092c665cb96027883a597e0d28d519f39f52
alex <surkov.alex@gmail.com>
2015-11-22 14:15:26 +0300

    drivers/amlogic/display/vout: Fix detection of current display mode in uboot_display_already() when CONFIG_AML_VOUT_FRAMERATE_AUTOMATION is enabled

--- drivers/amlogic/display/vout/tvoutc.c
+++ drivers/amlogic/display/vout/tvoutc.c
@@ -252,6 +252,16 @@ static void set_tvmode_misc(tvmode_t mode)
     set_vmode_clk(mode);
 }
 
+static const reg_t * tvregs_setting_mode(tvmode_t mode)
+{
+    int i = 0;
+    for(i = 0; i < ARRAY_SIZE(tvregsTab); i++) {
+        if(mode == tvregsTab[i].tvmode)
+            return tvregsTab[i].reg_setting;
+    }
+    return NULL;
+}
+
 /*
  * uboot_display_already() uses to judge whether display has already
  * be set in uboot.
@@ -265,8 +275,8 @@ static int uboot_display_already(tvmode_t mode)
         return 1;
     else
         return 0;
-    /*
-    const  reg_t *s = tvregsTab[mode];
+
+    const  reg_t *s = tvregs_setting_mode(mode);
     unsigned int pxcnt_tab = 0;
     unsigned int lncnt_tab = 0;
 
@@ -286,7 +296,7 @@ static int uboot_display_already(tvmode_t mode)
     } else {
         return 0;
     }
-    */
+
 }
 
 #if (MESON_CPU_TYPE >= MESON_CPU_TYPE_MESON8)
@@ -409,15 +419,6 @@ static void cvbs_performance_enhancement(tvmode_t mode)
 
 static DEFINE_MUTEX(setmode_mutex);
 
-static const reg_t * tvregs_setting_mode(tvmode_t mode)
-{
-    int i = 0;
-    for(i = 0; i < ARRAY_SIZE(tvregsTab); i++) {
-        if(mode == tvregsTab[i].tvmode)
-            return tvregsTab[i].reg_setting;
-    }
-    return NULL;
-}
 
 const static tvinfo_t * tvinfo_mode(tvmode_t mode)
 {
